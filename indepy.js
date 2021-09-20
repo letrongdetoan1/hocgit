c341787723ab451cb39cfaf2a05bc6722c20cfb8
letrongdetoan1 <letrongdetoan@gmail.com>
2021-09-20 08:08:23 +0700

    first commit

---
+++ indepy.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/f8', function(req, res) {
+    var a = 1;
+    var b = 2;
+    res.send('Xin chao');
+})
+app.listen(port, function(req, res) {
+    console.log('ok')
+})
